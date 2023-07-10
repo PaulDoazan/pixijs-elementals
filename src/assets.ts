acf7e710f6737aafb5fd063cb6a635467773a29d
pdoazan <paul.doazan@gmail.com>
2023-07-10 16:10:55 +0200

    Fix load assets

--- src/assets.ts
+++ src/assets.ts
@@ -10,7 +10,6 @@ export const manifest: ResolverManifest = {
                 "Adele Fish 1": "./adeleFish_1.png",
                 "Adele Fish 2": "./adeleFish_2.png",
                 "Adele Fish 3": "./adeleFish_3.png",
-                "Sand": "./sandBeach.jpg",
                 "Bottom": "./dark-bottom.jpg",
                 "Water": "./waves.png",
                 "displacement_map_repeat": "./displacementMap.jpg",
